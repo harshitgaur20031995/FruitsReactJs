3365f02f8c825cc6674ac1466f54dcfeb1e2fe57
harshitgaur20031995 <harshitr0025@gmail.com>
2021-02-16 17:16:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TestDemo from './component/TestDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <TestDemo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/TestDemo.js
@@ -0,0 +1,172 @@
+import React from 'react'
+import logo from '../minus.svg'
+
+const TestDemo = () => {
+    return (
+        <div className="container">
+            <div className="row my-3">
+                <div className="col-sm-6">
+
+                    <div className="float-start">
+                        <img height="20" width="20" src={logo} alt="s"></img>
+                    </div>
+                    <div className="mx-4 float-start">
+                        <h4>Healthy Fruites</h4>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="row yellow-background">
+            <div className="col-sm-6">
+                   
+                   </div>
+                <div className="col-sm-6 mt-4">
+                    <h2>Lorem Impswm</h2>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </div>
+                
+            </div>
+
+            <div className="row">
+                <div className="col-md-6 mt-3">
+                    <h2>Lorem Impswm</h2>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+
+                </div>
+                <div className="col-sm-6 d-flex justify-content-end">
+                    <img src="https://previews.123rf.com/images/photomaru/photomaru1510/photomaru151000012/46658347-pile-of-various-fresh-fruits-over-white-background.jpg" height="200"></img>
+                </div>
+
+            </div>
+            <div className="row green-background">
+            <div className="col-sm-6"></div>
+                <div className="col-sm-6 mt-4">
+                    <h2>Lorem Impswm</h2>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </div>
+               
+            </div>
+
+            <div className="card mt-n1">
+                <div className="card-body">
+                    <h3>Nutrition fact</h3>
+                    <p>serving size
+                        <span className="float-end">100g</span>
+                    </p>
+                    <div className="bold"></div>
+
+                    <h4 className="float-none">
+                        Amount per serving
+                    </h4>
+                    <div className="d-flex justify-content-between">
+                        <h1>Calories</h1>
+                        <h1>89</h1>
+                    </div>
+
+                    <div className="bold"></div>
+
+                    <div className="d-flex justify-content-end"> % Daily value * </div>
+                    <hr />
+                    <div className="d-flex justify-content-between">
+                        <h4>Total fat 0.3g</h4>
+                        <h4>0%</h4>
+                    </div>
+                    <hr />
+                    <div className="d-flex justify-content-between">
+                        <p>Saturated fat 0.1g</p>
+                        <p>0%</p>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between">
+                        <h4>Sodium 1mg</h4>
+                        <h4>0%</h4>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between">
+                        <h4>Total Carbohydrate 23g</h4>
+                        <h4>8%</h4>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between">
+                        <p>Ditary fiber 2.6g</p>
+                        <p>9%</p>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between">
+                        <p>Sugar 12g</p>
+                        <p></p>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between">
+                        <h4>Protin 1.1g</h4>
+                        <h4>2%</h4>
+                    </div>
+
+                    <div className="bold"></div>
+
+                    <div className="d-flex justify-content-between">
+                        <p>Vitamin D 0.00mcg</p>
+                        <p>0%</p>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between">
+                        <p>Calcium  5.00mg</p>
+                        <p>0%</p>
+                    </div>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between">
+                        <p>Iron  0.26mg</p>
+                        <p>1%</p>
+                    </div>
+
+                    <hr/>
+
+                    <div className="d-flex justify-content-between"> 
+                    <p>Potasssium  358mg</p>
+                    <p>8%</p>
+                   </div>
+                
+                   <div className="bold"></div>
+
+
+                </div>
+            </div>
+
+            <ul className="my-5">
+                <li className="text-success"><h4>Delibering with love and care for tou to relish</h4></li>
+            </ul>
+
+            <div className="bold my-4"></div>
+
+            <h5>About Demo Group</h5>
+            <p>Leading the crops division for demo group Indias best branded frut company and a market leader in controled cultivation and marketing of high-quality fruits to domestic and international customers in a socially responsible manner.</p>
+            <p>We arn happy to serve fresh & delicious fruits</p>
+
+            <div className="bold my-5"></div>
+
+
+
+
+        </div>
+    )
+}
+
+export default TestDemo
